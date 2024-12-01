3a00b0c14d49f2037f34806da69456291147a2de
Aditya Navakhande <153051566+imadityaim@users.noreply.github.com>
2024-12-01 18:50:44 +0530

    vision.py

--- vision.py
+++ vision.py
@@ -29,7 +29,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-submit = st.button("Tell me about the image")
+submit = st.button("Result")
 
 # If the submit button is clicked
 if submit:
